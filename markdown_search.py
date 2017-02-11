0e3421407d1f05eb1d4d8ecc2e990d8a637f7d2e
bigzhu <vermiliondun@gmail.com>
2017-02-12 00:30:25 +0800

    加入安装件, 搜索路径传入进来

--- markdown_search.py
+++ markdown_search.py
@@ -11,39 +11,33 @@ import time
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-import ConfigParser
-config = ConfigParser.ConfigParser()
-with open('conf.ini', 'r') as cfg_file:
-    config.readfp(cfg_file)
-    PATH = config.get('data', 'path')
-
 RESULT_FILE = 'search.md'
 
 NOT_IN = [RESULT_FILE]
 
 
-def search(name):
+def search(path, name):
     mds = {}
-    for md in os.listdir(PATH):
+    for md in os.listdir(path):
         md = os.path.basename(md)
-        if os.path.isdir(PATH + md):  # 跳过目录
+        if os.path.isdir(path + md):  # 跳过目录
             continue
         if md in NOT_IN:  # 跳过目录特定文件名
             continue
 
         if(fnmatch.fnmatchcase(md.upper(), ('*%s*' % name).upper())):
             # 取文件修改时间
-            modify_time = time.localtime(os.path.getmtime(PATH + md))
+            modify_time = time.localtime(os.path.getmtime(path + md))
             mds[md] = modify_time
         # 按时间排序
     mds = sorted(mds.items(), key=lambda by: by[1], reverse=True)
     return mds
 
 
-def write(mds, name):
+def write(path, name, mds):
     '''
     '''
-    f = open(PATH + RESULT_FILE, 'w')
+    f = open(path + RESULT_FILE, 'w')
     # print >>f, '%nohtml'
     for md in mds:
         print >>f, '%s' % md[0]
@@ -54,12 +48,16 @@ def write(mds, name):
 def main():
     args = sys.argv
     if(len(args) == 1):
-        print '请输入要查找的 wiki 关键字'
+        print '请输入要查找的目录和 wiki 关键字'
         exit()
     elif(len(args) == 2):
-        name = args[1]
-        mds = search(name)
-        write(mds, name)
+        print '请输入要查找的目录和 wiki 关键字'
+        exit()
+    elif(len(args) == 3):
+        path = args[1]
+        name = args[2]
+        mds = search(path, name)
+        write(path, name, mds)
 
 if __name__ == '__main__':
     main()
